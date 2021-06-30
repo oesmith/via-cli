07f5d6509b6fc922e27047800ce75f5d6a338a7e
Olly Smith <olly.smith@gmail.com>
2021-06-30 19:11:24 +0100

    Add getopt parsing for command-line arguments.

--- README.md
+++ README.md
@@ -6,20 +6,42 @@ Command-line interface for managing VIA keyboard firmware
 ```
 Usage: via [command] [args...]
 
-Basic:
-- devices
-- version [vendor:product]
-- uptime [vendor:product]
+Commands:
+  devices
+  version -d [vendor:product]
+  uptime -d [vendor:product]
 Keymap:
-- get_keycode [vendor:product] [layer] [row] [column]
-- set_keycode [vendor:product] [layer] [row] [column] [keycode]
+  get_keycode -d [vendor:product] -l [layer] -r [row] -c [column]
+  set_keycode -d [vendor:product] -l [layer] -r [row] -c [column] -k [keycode]
 RGB:
-- get_rgb_brightness [vendor:product]
-- get_rgb_mode [vendor:product]
-- get_rgb_speed [vendor:product]
-- get_rgb_colour [vendor:product]
-- set_rgb_brightness [vendor:product] [brightness]
-- set_rgb_mode [vendor:product] [mode]
-- set_rgb_speed [vendor:product] [speed]
-- set_rgb_colour [vendor:product] [hue] [saturation]
+  get_rgb_brightness -d [vendor:product]
+  get_rgb_mode -d [vendor:product]
+  get_rgb_speed -d [vendor:product]
+  get_rgb_colour -d [vendor:product]
+  set_rgb_brightness -d [vendor:product] -b [brightness]
+  set_rgb_mode -d [vendor:product] -m [mode]
+  set_rgb_speed -d [vendor:product] -s [speed]
+  set_rgb_colour -d [vendor:product] -h [hue] -S [saturation]
+
+Flags:
+-d VENDOR:PRODUCT
+   Select a device to command. Use 'devices' to enumerate available devices.
+-b [brightness] (0-255, default: 0)
+   RGB brightness.
+-m [mode] (0-255, default: 0)
+   RGB effect mode.
+-s [speed] (0-255, default: 0)
+   RGB effect speed.
+-h [hue] (0-255, default: 0)
+   RGB colour hue.
+-S [saturation] (0-255, default: 0)
+   RGB colour saturation.
+-l [layer] (0-255, default: 0)
+   Key layer.
+-r [row] (0-255, default: 0)
+   Key row.
+-c [column] (0-255, default: 0)
+   Key column.
+-k [keycode] (0-255, default: 0)
+   Hexadecimal keycode.
 ```
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <getopt.h>
 #include <hidapi.h>
 
 #include "commands.h"
@@ -13,73 +14,104 @@
 #define RAW_USAGE_PAGE 0xff60
 #define RAW_USAGE_ID 0x61
 
-#define CMD(device, data) command(device, data, sizeof(data))
-
 #undef DEBUG
 
+uint8_t packet[PACKET_SIZE];
 
-static unsigned char PACKET[PACKET_SIZE];
+hid_device *flag_device = NULL;
+uint8_t flag_row = 0;
+uint8_t flag_column = 0;
+uint8_t flag_layer = 0;
+uint8_t flag_brightness = 0;
+uint8_t flag_mode = 0;
+uint8_t flag_speed = 0;
+uint8_t flag_hue = 0;
+uint8_t flag_saturation = 0;
+unsigned short flag_keycode = 0;
 
 void dump_packet() {
 #ifdef DEBUG
   printf("__ __ __ __ __ __ __ __\n");
-  printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", PACKET[0], PACKET[1],
-         PACKET[2], PACKET[3], PACKET[4], PACKET[5], PACKET[6], PACKET[7]);
-  printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", PACKET[8], PACKET[9],
-         PACKET[10], PACKET[11], PACKET[12], PACKET[13], PACKET[14],
-         PACKET[15]);
-  printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", PACKET[16], PACKET[17],
-         PACKET[18], PACKET[19], PACKET[20], PACKET[21], PACKET[22],
-         PACKET[23]);
-  printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", PACKET[24], PACKET[25],
-         PACKET[26], PACKET[27], PACKET[28], PACKET[29], PACKET[30],
-         PACKET[31]);
+  printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", packet[0], packet[1],
+         packet[2], packet[3], packet[4], packet[5], packet[6], packet[7]);
+  printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", packet[8], packet[9],
+         packet[10], packet[11], packet[12], packet[13], packet[14],
+         packet[15]);
+  printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", packet[16], packet[17],
+         packet[18], packet[19], packet[20], packet[21], packet[22],
+         packet[23]);
+  printf("%02x %02x %02x %02x %02x %02x %02x %02x\n", packet[24], packet[25],
+         packet[26], packet[27], packet[28], packet[29], packet[30],
+         packet[31]);
 #endif
 }
 
-unsigned char *command(hid_device *device, unsigned char *data, int len) {
-  memset(PACKET, 0, PACKET_SIZE);
-  memcpy(PACKET + 1, data, len);
+void send(uint8_t *data, int len) {
+  if (flag_device == NULL) {
+    fprintf(stderr, "--device flag required.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(packet, 0, PACKET_SIZE);
+  memcpy(packet + 1, data, len);
 
   dump_packet();
 
-  if (hid_write(device, PACKET, PACKET_SIZE) != PACKET_SIZE) {
+  if (hid_write(flag_device, packet, PACKET_SIZE) != PACKET_SIZE) {
     perror("Error writing request\n");
-    return NULL;
+    exit(EXIT_FAILURE);
   }
 
-  if (hid_read_timeout(device, PACKET, PACKET_SIZE, 500) != PACKET_SIZE) {
+  if (hid_read_timeout(flag_device, packet, PACKET_SIZE, 500) != PACKET_SIZE) {
     perror("Error reading response\n");
-    return NULL;
+    exit(EXIT_FAILURE);
   }
 
   dump_packet();
-
-  return PACKET;
 }
 
-int help() {
+void help() {
   printf("Usage: via [command] [args...]\n");
-  printf("\nBasic:\n");
-  printf("- devices\n");
-  printf("- version [vendor:product]\n");
-  printf("- uptime [vendor:product]\n");
+  printf("\nCommands:\n");
+  printf("  devices\n");
+  printf("  version -d [vendor:product]\n");
+  printf("  uptime -d [vendor:product]\n");
   printf("Keymap:\n");
-  printf("- get_keycode [vendor:product] [layer] [row] [column]\n");
-  printf("- set_keycode [vendor:product] [layer] [row] [column] [keycode]\n");
+  printf("  get_keycode -d [vendor:product] -l [layer] -r [row] -c [column]\n");
+  printf("  set_keycode -d [vendor:product] -l [layer] -r [row] -c [column] -k [keycode]\n");
   printf("RGB:\n");
-  printf("- get_rgb_brightness [vendor:product]\n");
-  printf("- get_rgb_mode [vendor:product]\n");
-  printf("- get_rgb_speed [vendor:product]\n");
-  printf("- get_rgb_colour [vendor:product]\n");
-  printf("- set_rgb_brightness [vendor:product] [brightness]\n");
-  printf("- set_rgb_mode [vendor:product] [mode]\n");
-  printf("- set_rgb_speed [vendor:product] [speed]\n");
-  printf("- set_rgb_colour [vendor:product] [hue] [saturation]\n");
-  return 0;
+  printf("  get_rgb_brightness -d [vendor:product]\n");
+  printf("  get_rgb_mode -d [vendor:product]\n");
+  printf("  get_rgb_speed -d [vendor:product]\n");
+  printf("  get_rgb_colour -d [vendor:product]\n");
+  printf("  set_rgb_brightness -d [vendor:product] -b [brightness]\n");
+  printf("  set_rgb_mode -d [vendor:product] -m [mode]\n");
+  printf("  set_rgb_speed -d [vendor:product] -s [speed]\n");
+  printf("  set_rgb_colour -d [vendor:product] -h [hue] -S [saturation]\n");
+  printf("\nFlags:\n");
+  printf("-d VENDOR:PRODUCT\n");
+  printf("   Select a device to command. Use 'devices' to enumerate available devices.\n");
+  printf("-b [brightness] (0-255, default: 0)\n");
+  printf("   RGB brightness.\n");
+  printf("-m [mode] (0-255, default: 0)\n");
+  printf("   RGB effect mode.\n");
+  printf("-s [speed] (0-255, default: 0)\n");
+  printf("   RGB effect speed.\n");
+  printf("-h [hue] (0-255, default: 0)\n");
+  printf("   RGB colour hue.\n");
+  printf("-S [saturation] (0-255, default: 0)\n");
+  printf("   RGB colour saturation.\n");
+  printf("-l [layer] (0-255, default: 0)\n");
+  printf("   Key layer.\n");
+  printf("-r [row] (0-255, default: 0)\n");
+  printf("   Key row.\n");
+  printf("-c [column] (0-255, default: 0)\n");
+  printf("   Key column.\n");
+  printf("-k [keycode] (0-255, default: 0)\n");
+  printf("   Hexadecimal keycode.\n");
 }
 
-int devices() {
+void devices() {
   struct hid_device_info *enumeration = hid_enumerate(0, 0);
   struct hid_device_info *device_info = enumeration;
   while (device_info != NULL) {
@@ -92,187 +124,96 @@ int devices() {
     device_info = device_info->next;
   }
   hid_free_enumeration(enumeration);
-  return 0;
 }
 
-int version(hid_device *device) {
-  if (device == NULL) {
-    return 1;
-  }
-
-  unsigned char req[] = {id_get_protocol_version};
-  unsigned char *res = CMD(device, req);
-  if (res != NULL) {
-    printf("Version: %u\n", res[1] << 8 | res[2]);
-  }
-
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void version() {
+  send((uint8_t[]){id_get_protocol_version}, 1);
+  printf("Version: %u\n", packet[1] << 8 | packet[2]);
 }
 
-int uptime(hid_device *device) {
-  if (device == NULL) {
-    return 1;
-  }
-
-  unsigned char req[] = {id_get_keyboard_value, id_uptime};
-  unsigned char *res = CMD(device, req);
-  if (res != NULL) {
-    uint32_t uptime = res[2] << 24 | res[3] << 16 | res[4] << 8 | res[5];
-    printf("Uptime: %u\n", uptime);
-  }
-
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void uptime() {
+  send((uint8_t[]){id_get_keyboard_value, id_uptime}, 2);
+  uint32_t uptime =
+      packet[2] << 24 | packet[3] << 16 | packet[4] << 8 | packet[5];
+  printf("Uptime: %u\n", uptime);
 }
 
-int get_rgb_brightness(hid_device *device) {
-  if (device == NULL) {
-    return 1;
-  }
-
-  unsigned char req[] = {id_lighting_get_value, id_qmk_rgblight_brightness};
-  unsigned char *res = CMD(device, req);
-  if (res != NULL) {
-    printf("Brightness: %u\n", res[2]);
-  }
-
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void get_rgb_brightness() {
+  send((uint8_t[]){id_lighting_get_value, id_qmk_rgblight_brightness}, 2);
+  printf("Brightness: %hhu\n", packet[2]);
 }
 
-int get_rgb_mode(hid_device *device) {
-  if (device == NULL) {
-    return 1;
-  }
-
-  unsigned char req[] = {id_lighting_get_value, id_qmk_rgblight_effect};
-  unsigned char *res = CMD(device, req);
-  if (res != NULL) {
-    printf("Mode: %u\n", res[2]);
-  }
-
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void get_rgb_mode() {
+  send((uint8_t[]){id_lighting_get_value, id_qmk_rgblight_effect}, 2);
+  printf("Mode: %hhu\n", packet[2]);
 }
 
-int get_rgb_speed(hid_device *device) {
-  if (device == NULL) {
-    return 1;
-  }
-
-  unsigned char req[] = {id_lighting_get_value, id_qmk_rgblight_effect_speed};
-  unsigned char *res = CMD(device, req);
-  if (res != NULL) {
-    printf("Speed: %u\n", res[2]);
-  }
-
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void get_rgb_speed() {
+  send((uint8_t[]){id_lighting_get_value, id_qmk_rgblight_effect_speed}, 2);
+  printf("Speed: %hhu\n", packet[2]);
 }
 
-int get_rgb_colour(hid_device *device) {
-  if (device == NULL) {
-    return 1;
-  }
-
-  unsigned char req[] = {id_lighting_get_value, id_qmk_rgblight_color};
-  unsigned char *res = CMD(device, req);
-  if (res != NULL) {
-    printf("Hue: %u\n", res[2]);
-    printf("Saturation: %u\n", res[3]);
-  }
-
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void get_rgb_colour() {
+  send((uint8_t[]){id_lighting_get_value, id_qmk_rgblight_color}, 2);
+  printf("Hue: %hhu\n", packet[2]);
+  printf("Saturation: %hhu\n", packet[3]);
 }
 
-int set_rgb_brightness(hid_device *device, unsigned char brightness) {
-  if (device == NULL) {
-    return 1;
-  }
-  unsigned char req[] = {id_lighting_set_value, id_qmk_rgblight_color,
-                         brightness};
-  unsigned char *res = CMD(device, req);
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void set_rgb_brightness() {
+  send((uint8_t[]){id_lighting_set_value, id_qmk_rgblight_color,
+                   flag_brightness},
+       3);
+  printf("Brightness: %hhu\n", packet[2]);
 }
 
-int set_rgb_mode(hid_device *device, unsigned char mode) {
-  if (device == NULL) {
-    return 1;
-  }
-  unsigned char req[] = {id_lighting_set_value, id_qmk_rgblight_effect, mode};
-  unsigned char *res = CMD(device, req);
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void set_rgb_mode() {
+  send((uint8_t[]){id_lighting_set_value, id_qmk_rgblight_effect, flag_mode},
+       3);
+  printf("Mode: %hhu\n", packet[2]);
 }
 
-int set_rgb_speed(hid_device *device, unsigned char speed) {
-  if (device == NULL) {
-    return 1;
-  }
-  unsigned char req[] = {id_lighting_set_value, id_qmk_rgblight_effect_speed,
-                         speed};
-  unsigned char *res = CMD(device, req);
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void set_rgb_speed() {
+  send((uint8_t[]){id_lighting_set_value, id_qmk_rgblight_effect_speed,
+                   flag_speed},
+       3);
+  printf("Speed: %hhu\n", packet[2]);
 }
 
-int set_rgb_colour(hid_device *device, unsigned char hue,
-                   unsigned char saturation) {
-  if (device == NULL) {
-    return 1;
-  }
-  unsigned char req[] = {id_lighting_set_value, id_qmk_rgblight_color, hue,
-                         saturation};
-  unsigned char *res = CMD(device, req);
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void set_rgb_colour() {
+  send((uint8_t[]){id_lighting_set_value, id_qmk_rgblight_color, flag_hue,
+                   flag_saturation},
+       4);
+  printf("Hue: %hhu\n", packet[2]);
+  printf("Saturation: %hhu\n", packet[3]);
 }
 
-int get_keycode(hid_device *device, unsigned char layer, unsigned char row,
-                unsigned char column) {
-  if (device == NULL) {
-    return 1;
-  }
-  unsigned char req[] = {id_dynamic_keymap_get_keycode, layer, row, column};
-  unsigned char *res = CMD(device, req);
-  if (res != NULL) {
-    printf("Layer: %hhu Row: %hhu Column: %hhu\n", res[1], res[2], res[3]);
-    unsigned short keycode = res[4] << 8 | res[5];
-    char* keyname = (keycode < sizeof(qmk_keycodes)) ? qmk_keycodes[keycode] : "OTHER";
-    printf("Keycode: 0x%hx (%s)\n", keycode, keyname);
-  }
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void get_keycode() {
+  send((uint8_t[]){id_dynamic_keymap_get_keycode, flag_layer, flag_row,
+                   flag_column},
+       4);
+  printf("Layer: %hhu Row: %hhu Column: %hhu\n", packet[1], packet[2],
+         packet[3]);
+  unsigned short keycode = packet[4] << 8 | packet[5];
+  char *keyname =
+      (keycode < sizeof(qmk_keycodes)) ? qmk_keycodes[keycode] : "OTHER";
+  printf("Keycode: 0x%hx (%s)\n", keycode, keyname);
 }
 
-int set_keycode(hid_device *device, unsigned char layer, unsigned char row,
-                unsigned char column, unsigned short keycode) {
-  if (device == NULL) {
-    return 1;
-  }
-  unsigned char req[] = {id_dynamic_keymap_set_keycode,
-                         layer,
-                         row,
-                         column,
-                         keycode >> 8,
-                         keycode & 0xff};
-  unsigned char *res = CMD(device, req);
-  if (res != NULL) {
-    printf("Layer: %hhu Row: %hhu Column: %hhu\n", res[1], res[2], res[3]);
-    printf("Keycode: 0x%hx\n", res[4] << 8 | res[5]);
-  }
-  hid_close(device);
-  return res != NULL ? 0 : 1;
+void set_keycode() {
+  send((uint8_t[]){id_dynamic_keymap_set_keycode, flag_layer, flag_row,
+                   flag_column, flag_keycode >> 8, flag_keycode & 0xff},
+       6);
+  unsigned short keycode = packet[4] << 8 | packet[5];
+  char *keyname =
+      (keycode < sizeof(qmk_keycodes)) ? qmk_keycodes[keycode] : "OTHER";
+  printf("Keycode: 0x%hx (%s)\n", keycode, keyname);
 }
 
-hid_device *open_device(char *id) {
+void open_device(char *id) {
   unsigned short vendor_id, product_id;
   if (2 != sscanf(id, "%hx:%hx", &vendor_id, &product_id)) {
     printf("Cannot parse ID\n");
-    return NULL;
+    exit(EXIT_FAILURE);
   }
 
   struct hid_device_info *enumeration = hid_enumerate(0, 0);
@@ -288,38 +229,39 @@ hid_device *open_device(char *id) {
   }
 
   if (device_info == NULL) {
-    printf("No such device\n");
-    return NULL;
+    fprintf(stderr, "No such device: %s\n", id);
+    exit(EXIT_FAILURE);
   }
 
-  hid_device *device = hid_open_path(device_info->path);
-  if (device == NULL) {
+  flag_device = hid_open_path(device_info->path);
+  if (flag_device == NULL) {
     perror("Cannot open device\n");
-    return NULL;
+    exit(EXIT_FAILURE);
   }
 
   hid_free_enumeration(enumeration);
-
-  return device;
 }
 
-unsigned char u8(char *arg) {
-  unsigned char ret = 0;
-  if (sscanf(arg, "%hhu", &ret) != 1) {
-    fprintf(stderr, "Can't parse argument: %s\n", arg);
+void u8(char *arg, uint8_t *dest, char *name) {
+  if (sscanf(arg, "%hhu", dest) != 1) {
+    fprintf(stderr, "Invalid %s: %s\n", name, arg);
+    exit(EXIT_FAILURE);
   }
-  return ret;
 }
 
-unsigned short x16(char *arg) {
-  unsigned short ret = 0;
-  if (sscanf(arg, "%hx", &ret) != 1) {
-    fprintf(stderr, "Can't parse argument: %s\n", arg);
+void x16(char *arg, unsigned short *dest, char *name) {
+  if (sscanf(arg, "%hx", dest) != 1) {
+    fprintf(stderr, "Invalid %s: %s\n", name, arg);
+    exit(EXIT_FAILURE);
   }
-  return ret;
 }
 
-void cleanup() { hid_exit(); }
+void cleanup() {
+  if (flag_device != NULL) {
+    hid_close(flag_device);
+  }
+  hid_exit();
+}
 
 int main(int argc, char **argv) {
   if (0 > hid_init()) {
@@ -328,39 +270,85 @@ int main(int argc, char **argv) {
   }
   atexit(cleanup);
 
-  memset(PACKET, 0, PACKET_SIZE);
+  char *cmd = NULL;
 
-  if (argc == 1) {
-    return help();
-  } else if (strcmp(argv[1], "devices") == 0) {
-    return devices();
-  } else if (strcmp(argv[1], "version") == 0 && argc == 3) {
-    return version(open_device(argv[2]));
-  } else if (strcmp(argv[1], "uptime") == 0 && argc == 3) {
-    return uptime(open_device(argv[2]));
-  } else if (strcmp(argv[1], "get_rgb_brightness") == 0 && argc == 3) {
-    return get_rgb_brightness(open_device(argv[2]));
-  } else if (strcmp(argv[1], "get_rgb_mode") == 0 && argc == 3) {
-    return get_rgb_mode(open_device(argv[2]));
-  } else if (strcmp(argv[1], "get_rgb_speed") == 0 && argc == 3) {
-    return get_rgb_speed(open_device(argv[2]));
-  } else if (strcmp(argv[1], "get_rgb_colour") == 0 && argc == 3) {
-    return get_rgb_colour(open_device(argv[2]));
-  } else if (strcmp(argv[1], "set_rgb_brightness") == 0 && argc == 4) {
-    return set_rgb_brightness(open_device(argv[2]), u8(argv[3]));
-  } else if (strcmp(argv[1], "set_rgb_mode") == 0 && argc == 4) {
-    return set_rgb_mode(open_device(argv[2]), u8(argv[3]));
-  } else if (strcmp(argv[1], "set_rgb_speed") == 0 && argc == 4) {
-    return set_rgb_speed(open_device(argv[2]), u8(argv[3]));
-  } else if (strcmp(argv[1], "set_rgb_colour") == 0 && argc == 5) {
-    return set_rgb_colour(open_device(argv[2]), u8(argv[3]), u8(argv[4]));
-  } else if (strcmp(argv[1], "get_keycode") == 0 && argc == 6) {
-    return get_keycode(open_device(argv[2]), u8(argv[3]), u8(argv[4]),
-                       u8(argv[5]));
-  } else if (strcmp(argv[1], "set_keycode") == 0 && argc == 7) {
-    return set_keycode(open_device(argv[2]), u8(argv[3]), u8(argv[4]),
-                       u8(argv[5]), x16(argv[6]));
-  } else {
-    return help();
+  int opt;
+  while ((opt = getopt(argc, argv, "-d:m:s:b:h:S:r:c:l:k:")) != -1) {
+    switch (opt) {
+    case 1:
+      if (cmd != NULL) {
+        help();
+        exit(EXIT_FAILURE);
+      }
+      cmd = optarg;
+      break;
+    case 'd':
+      open_device(optarg);
+      break;
+    case 'm':
+      u8(optarg, &flag_mode, "mode");
+      break;
+    case 's':
+      u8(optarg, &flag_speed, "speed");
+      break;
+    case 'b':
+      u8(optarg, &flag_brightness, "brightness");
+      break;
+    case 'h':
+      u8(optarg, &flag_hue, "hue");
+      break;
+    case 'S':
+      u8(optarg, &flag_saturation, "saturation");
+      break;
+    case 'r':
+      u8(optarg, &flag_row, "row");
+      break;
+    case 'c':
+      u8(optarg, &flag_column, "column");
+      break;
+    case 'l':
+      u8(optarg, &flag_layer, "layer");
+      break;
+    case 'k':
+      x16(optarg, &flag_keycode, "keycode");
+      break;
+    default:
+      help();
+      exit(EXIT_FAILURE);
+    }
   }
+
+  if (cmd == NULL || strcmp(cmd, "help") == 0) {
+    help();
+  } else if (strcmp(cmd, "devices") == 0) {
+    devices();
+  } else if (strcmp(cmd, "version") == 0) {
+    version();
+  } else if (strcmp(cmd, "uptime") == 0) {
+    uptime();
+  } else if (strcmp(cmd, "get_rgb_brightness") == 0) {
+    get_rgb_brightness();
+  } else if (strcmp(cmd, "get_rgb_mode") == 0) {
+    get_rgb_mode();
+  } else if (strcmp(cmd, "get_rgb_speed") == 0) {
+    get_rgb_speed();
+  } else if (strcmp(cmd, "get_rgb_colour") == 0) {
+    get_rgb_colour();
+  } else if (strcmp(cmd, "set_rgb_brightness") == 0) {
+    set_rgb_brightness();
+  } else if (strcmp(cmd, "set_rgb_mode") == 0) {
+    set_rgb_mode();
+  } else if (strcmp(cmd, "set_rgb_speed") == 0) {
+    set_rgb_speed();
+  } else if (strcmp(cmd, "set_rgb_colour") == 0) {
+    set_rgb_colour();
+  } else if (strcmp(cmd, "get_keycode") == 0) {
+    get_keycode();
+  } else if (strcmp(cmd, "set_keycode") == 0) {
+    set_keycode();
+  } else {
+    help();
+  }
+
+  exit(EXIT_SUCCESS);
 }
